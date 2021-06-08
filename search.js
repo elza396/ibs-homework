8d61c7c2e513e3536dfc41ec8b2e55b6129a0333
elza396 <elza396@yandex.ru>
2021-06-09 01:02:15 +0500

    chore: some small fixes

--- search.js
+++ search.js
@@ -1,12 +1,11 @@
 var input = document.querySelector('.search-form__input');
 input.addEventListener('input', searchItem);
 function searchItem(e) {
-    searchText = e.target.value.toLowerCase();
-    console.log(searchText);
-    cards = document.querySelectorAll('.card');
+    var searchText = e.target.value.toLowerCase();
+    var cards = document.querySelectorAll('.card');
     for (var i = 0; i < cards.length; i++) {
         var itemName = cards[i].querySelector('.card_title').textContent;
-        if (itemName.toLowerCase().indexOf(searchText) > -1) {
+        if (itemName.toLowerCase().indexOf(searchText) !== -1) {
             cards[i].style.display = "";
         } else {
             cards[i].style.display = "none";
