187073b6a71f1911606bbea7418b0249076abb57
elza396 <elza396@yandex.ru>
2021-06-16 20:48:23 +0500

    fix: search and add debounce

--- search.js
+++ search.js
@@ -1,14 +1,19 @@
 var input = document.querySelector('.search-form__input');
 input.addEventListener('input', searchItem);
+
 function searchItem(e) {
     var searchText = e.target.value.toLowerCase();
     var cards = document.querySelectorAll('.card');
-    for (var i = 0; i < cards.length; i++) {
-        var itemName = cards[i].querySelector('.card_title').textContent;
-        if (itemName.toLowerCase().indexOf(searchText) !== -1) {
-            cards[i].style.display = "";
-        } else {
-            cards[i].style.display = "none";
+    var delayTimer;
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function() {
+        for (var i = 0; i < cards.length; i++) {
+            var itemName = cards[i].querySelector('.card_title').textContent;
+            if (itemName.toLowerCase().indexOf(searchText) === -1) {
+                cards[i].classList.add('card_hidden');
+            } else {
+                cards[i].classList.remove('card_hidden');
+            }
         }
-    }
+    }, 1000)
 }
